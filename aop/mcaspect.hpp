a2c825cf0a949c8c9e8ba033c2c6c17a6484d79a
gxkey <ymiwli@hotmail.com>
2021-01-31 21:04:28 +0800

    rename some parameter types

--- aop/mcaspect.hpp
+++ aop/mcaspect.hpp
@@ -1,23 +1,51 @@
+/*
+ *
+MIT License
+
+Copyright (c) 2021 gxkey(Gaoxing Li)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef MC_ASPECT_HPP
 #define MC_ASPECT_HPP
+
+#include <concepts>
+
 
 namespace mc
 {
 
-template<typename Aspect, typename... Args>
-concept HasMemberBefore = requires (Aspect ap) { ap.before(std::declval<Args>()...); };
+template<typename Advice, typename... Args>
+concept HasMemberBefore = requires (Advice advice) { advice.before(std::declval<Args>()...); };
 
-template<typename Aspect, typename... Args>
-concept HasMemberAfter = requires (Aspect ap) { ap.after(std::declval<Args>()...); };
+template<typename Advice, typename... Args>
+concept HasMemberAfter = requires (Advice advice) { advice.after(std::declval<Args>()...); };
 
 template<typename Func>
-struct aspect
+struct aspect final
 {
 	aspect(Func&& func) : func_(std::forward<Func>(func))
 	{}
 
-	template<typename... Aspects, typename... Args>
-	void invoke(Args&&... args)
+	template<typename... Advice, typename... Args>
+	void invoke(Args&&... args) const
 	{
 		auto invoke_before = [&](auto&& ap) {
 			if constexpr (HasMemberBefore<decltype(ap), Args...>) 
@@ -30,9 +58,9 @@ struct aspect
 		};
 
 		int dummy;
-		(invoke_before(Aspects()), ...);
+		(invoke_before(Advice()), ...);
 		func_(std::forward<Args>(args)...);
-		(dummy = ... = (invoke_after(Aspects()), 0));
+		(dummy = ... = (invoke_after(Advice()), 0));
 	}
 
 private:
@@ -40,11 +68,11 @@ private:
 };
 
 
-template<typename... Aspects, typename... Args, typename Func>
+template<typename... Advice, typename... Args, typename Func>
 void make_aspect(Func&& func, Args&&... args)
 {
 	mc::aspect<Func> ap(std::forward<Func>(func));
-	ap.invoke<Aspects...>(std::forward<Args>(args)...);
+	ap.invoke<Advice...>(std::forward<Args>(args)...);
 }
 
 } // namespace mc
